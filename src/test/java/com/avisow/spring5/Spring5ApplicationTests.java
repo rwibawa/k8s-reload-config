52f2ca733542fcbe208abeda8b82ede657d7cfb8
Ryan Wibawa <ryan.wibawa@gmail.com>
2020-05-09 11:23:54 -0700

    Add API RestDocumentation.

--- src/test/java/com/avisow/spring5/Spring5ApplicationTests.java
+++ src/test/java/com/avisow/spring5/Spring5ApplicationTests.java
@@ -1,22 +1,47 @@
 package com.avisow.spring5;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
 @ExtendWith(SpringExtension.class)
 //  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Spring5ApplicationTests {
 
-	// Spring Boot will create a `WebTestClient` for you,
-	// already configure and ready to issue requests against "localhost:RANDOM_PORT"
-	@Autowired
+	@RegisterExtension
+	final RestDocumentationExtension restDocumentation = new RestDocumentationExtension ("custom");
+
 	private WebTestClient webTestClient;
+
+	@BeforeEach
+	public void setUp(ApplicationContext applicationContext,
+					  RestDocumentationContextProvider restDocumentation) {
+		this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext)
+				.configureClient()
+				.filter(documentationConfiguration(restDocumentation))
+				.build();
+	}
+
+	@Test
+	public void testHomepage() {
+		webTestClient
+				.get().uri("/")
+				.accept(MediaType.TEXT_PLAIN)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("Home page");
+	}
 
 	@Test
 	public void testHello() {
